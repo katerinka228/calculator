a5c4730007ff8067138d041ef95206123ef4cc0e
katerinka228 <varfolomeewa.catherine@yandex.ru>
2025-02-06 22:04:40 +0300

    работают все кнопки, очищение, удалить символ, степень, корень

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let screen = document.querySelector("#screen");
 let btn = document.querySelectorAll(".btn");
-let equal = document.querySelector('#equal');
+// let equal = document.querySelector('#equal');
 
 for (item of btn)
 {
@@ -23,7 +23,28 @@ for (item of btn)
     })
 }
 
-equal.onclick = function () {
+// equal.onclick = function () {
+//     screen.value = eval(screen.value);
+// };
+let equal = function ()
+{
     screen.value = eval(screen.value);
 };
 
+let sqrt = function ()
+{
+    screen.value = Math.sqrt(screen.value);
+};
+
+let pow = function ()
+{
+    screen.value = Math.pow(screen.value, 2);
+};
+
+let backSpace = function () {
+    screen.value = screen.value.substring(0, screen.value.length - 1);
+};
+
+let clearAll = function () {
+    screen.value = "";
+};
